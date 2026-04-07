e0e92ea76297d5504d151f62344cd4314f82a23d
Quintrix <joe_frank_31@yahoo.com>
2026-04-06 23:21:53 -0400

    push

--- gfx.js
+++ gfx.js
@@ -475,6 +475,7 @@ window.gfxCreation = async function(drive, mapString, lobbyMap, isRound) {
 };
 
 splash(1000);
+alert("load gfx-itemid.js");
 qdosScript("gfx-itemid.js");
 async function splash(durationMs) {
   _CURSOR=CURSOR; CURSOR=0;
