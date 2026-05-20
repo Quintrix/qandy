7ef7a581e63e0b11b8351cdc4906ecc529ee3147
Quintrix <joe_frank_31@yahoo.com>
2026-05-20 10:59:27 -0400

    push

--- qandy-keyboard.js
+++ qandy-keyboard.js
@@ -109,6 +109,7 @@ function updateKeyLabels() {
         } else if (typeof normalKeys === 'object' && normalKeys.hasOwnProperty(lookup)) {
           label = normalKeys[lookup];
         } else {
+        	
           label = base;
         }
       } else if (window.shiftActive) {
